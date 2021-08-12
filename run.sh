c2e86e5f9f17116941a6838cf0206e8119cf49fe
Charles Yang <Charles@0x7f.cc>
2021-08-12 16:02:38 +0800

    Fix bug

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-[ -e registry ] && (cd registry; git pull) || git clone https://git:$DN42GITTOKEN@git.dn42.dev/dn42/registry registry --depth 1 --single-branch
+[ -e registry ] && (cd registry; git pull) || git clone https://git:$1@git.dn42.dev/dn42/registry registry --depth 1 --single-branch
+
+mkdir -p data
 
 wget https://github.com/isjerryxiao/rushed_dn42_map/blob/pages/parsed.jsonl.bz2?raw=true -O data/table.jsonl.bz2
 
