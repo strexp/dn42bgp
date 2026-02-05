f4517ca66f2c3cec98d654f5888a2a997900bfcb
Yukari Chiba <i@0x7f.cc>
2026-02-05 11:45:20 +0800

    history: tracks more fields

--- core/history/config.py
+++ core/history/config.py
@@ -1,23 +1,28 @@
-from typing import List, Dict, TypedDict, Optional
+from typing import Dict, List, Optional, TypedDict
+
 
 class ConfigItem(TypedDict):
     dir: str
     key_field: str
     track_fields: List[str]
 
+
 class StateValue(TypedDict):
     val: Dict[str, str]
     start: str
+
 
 class TimelineItem(TypedDict):
     value: Dict[str, str]
     start: str
     end: str
 
+
 class AppState(TypedDict):
     last_commit: Optional[str]
     timeline_data: Dict[str, Dict[str, List[TimelineItem]]]
     active_states: Dict[str, Dict[str, StateValue]]
+
 
 CONFIG: Dict[str, ConfigItem] = {
     "aut-num": {
@@ -25,20 +30,12 @@ CONFIG: Dict[str, ConfigItem] = {
         "key_field": "aut-num",
         "track_fields": ["as-name", "mnt-by"],
     },
-    "dns": {
-        "dir": "dns", 
-        "key_field": "domain", 
-        "track_fields": ["mnt-by"]
-    },
-    "mntner": {
-        "dir": "mntner", 
-        "key_field": "mntner", 
-        "track_fields": []
-    },
+    "dns": {"dir": "dns", "key_field": "domain", "track_fields": ["mnt-by"]},
+    "mntner": {"dir": "mntner", "key_field": "mntner", "track_fields": ["mntner"]},
     "registry": {
-        "dir": "registry", 
-        "key_field": "registry", 
-        "track_fields": []
+        "dir": "registry",
+        "key_field": "registry",
+        "track_fields": ["registry"],
     },
     "organisation": {
         "dir": "organisation",
